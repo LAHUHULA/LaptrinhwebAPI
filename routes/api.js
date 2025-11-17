3e0f4e4f1254c96840e89145922b5ad4facdd831
lahu <lanh2211pc@gmail.com>
2025-11-17 10:56:07 +0700

    feat: demo GET user api

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 import express from "express";
+import { getAllUsers, getUserById } from "../controllers/userController.js";
+
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Welcome to API route 🚀" });
+  res.json({ message: "Welcome to API route" });
 });
+
+// Route: GET /api/users
+router.get("/users/", getAllUsers);
+
+// Route: GET /api/users/:id
+router.get("/users/:id", getUserById)
+
 
 export default router;
